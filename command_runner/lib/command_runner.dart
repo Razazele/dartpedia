5be27ddd0bbd0823c2e14d8e1c6b514f4302362e
Pablo <pablo_verdugoj@hotmail.com>
2026-09-17 16:03:07 -0300

    feat:Export arguments para que lo ocupen los otros paquetes de la dependencia command_runner

--- command_runner/lib/command_runner.dart
+++ command_runner/lib/command_runner.dart
@@ -2,7 +2,8 @@
 ///
 /// More dartdocs go here.
 library;
+//Allow other packages to import arguments
 
+export 'src/arguments.dart';
 export 'src/command_runner_base.dart';
-
 // TODO: Export any libraries intended for clients of this package.
