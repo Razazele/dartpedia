e44a8c0682ad2c3160ae0d701cc064126169a039
Pablo <pablo_verdugoj@hotmail.com>
2026-09-18 12:55:32 -0300

    Agregadas excepciones para cada caso en parse/analisis de los comandos

--- command_runner/lib/src/command_runner_base.dart
+++ command_runner/lib/src/command_runner_base.dart
@@ -50,9 +50,101 @@ class CommandRunner {
   }
 
   ArgResults parse(List<String> input) {
-    var results = ArgResults();
-    results.command = _commands[input.first];
+    ArgResults results = ArgResults();
+    if (input.isEmpty) return results;
+
+    //Arroja exception si el comando no es reconocido
+
+    if (_commands.containsKey(input.first)) {
+      results.command = _commands[input.first];
+      input = input.sublist(1);
+    } else {
+      throw ArgumentException(
+        'La primera palabra del input debe ser un comando.',
+        null,
+        input.first,
+      );
+    }
+
+    //Arroja una excepcion si multiples comandos son ingresados
+    if (results.command != null &&
+        input.isNotEmpty &&
+        _commands.containsKey(input.first)) {
+      throw ArgumentException(
+        'El input solo puede contener un comando. Obtenido ${input.first} y ${results.command!.name}.',
+        null,
+        input.first,
+      );
+    }
+
+    //Seccion : Manejar opciones, incluyendo flags
+    Map<Option, Object?> inputOptions = {};
+
+    int i = 0;
+    //Manejo de excepciones
+    while (i < input.length) {
+      if (input[i].startsWith('-')) {
+        var base = _removeDash(input[i]);
+        //Arroja una excepcion si el la opcion no es reconocida para el comando dado
+        var option = results.command!.options.firstWhere(
+          (option) => option.name == base || option.abbr == base,
+          orElse: () {
+            throw ArgumentException(
+              'Unknown option ${input[i]}',
+              results.command!.name,
+              input[i],
+            );
+          },
+        );
+        if (option.type == OptionType.option) {
+          //Arroja Excepcion si la opcion requiere un arg pero no se dio ninguno.
+          if (i + 1 >= input.length) {
+            throw ArgumentException(
+              'Opcion ${option.name} requiere un argumento',
+              results.command!.name,
+              option.name,
+            );
+          }
+        }
+        //Arroja excepcion si se obtuvo otra opcion
+        if (input[i + 1].startsWith('-')) {
+          throw ArgumentException(
+            'Opcion ${option.name} requiere un argumento. pero obtuvo otra opcion ${input[i + 1]}',
+            results.command!.name,
+            option.name,
+          );
+        }
+
+        var arg = input[i + 1];
+        inputOptions[option] = arg;
+        i++;
+      } else {
+        //Arroja Excepcion si se provee mas de un argumento
+        if (results.commandArg != null && results.commandArg!.isNotEmpty) {
+          throw ArgumentException(
+            'Comandos solo pueden tener hasta 1 argumento',
+            results.command!.name,
+            input[i],
+          );
+        }
+
+        results.commandArg = input[i];
+      }
+      i++;
+    }
+    results.options = inputOptions;
     return results;
+  }
+
+  String _removeDash(String input) {
+    if (input.startsWith('--')) {
+      return input.substring(2);
+    }
+    if (input.startsWith('-')) {
+      return input.substring(1);
+    }
+
+    return input;
   }
 
   String get usage {
