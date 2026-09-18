f7605e70abbf5ed14e0d427bec518b734744eafe
Pablo <pablo_verdugoj@hotmail.com>
2026-09-18 16:36:41 -0300

    Implementado onError en Cli.dart commandrunner

--- bin/cli.dart
+++ bin/cli.dart
@@ -2,10 +2,20 @@ import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
 void main(List<String> arguments) {
+  //Agregar metodo onError
   /* The cascade notation ..addCommand(...) calls addCommand 
   on the newly constructed 
   CommandRunner and returns that runner instance, enabling 
   concise method chaining before passing arguments to run().*/
-  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  var commandRunner = CommandRunner(
+    onError: (Object error) {
+      if (error is Error) {
+        throw error;
+      }
+      if (error is Exception) {
+        print(error);
+      }
+    },
+  )..addCommand(HelpCommand());
   commandRunner.run(arguments);
 }
